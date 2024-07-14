3bc0faebfb782ad7b6d5139c18497798d3991bda
Swapnil Hoskatti <shoskatti@gmail.com>
2024-07-14 04:36:53 -0400

    [Restored stashed changes after error]Started working on the Database. Users almost completed.

--- db/sql/create_tables.sql
+++ db/sql/create_tables.sql
@@ -1,43 +1,47 @@
-// CREATE POSTGRES DATABASE
+-- CREATE POSTGRES TABLES
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` SERIAL NOT NULL,
-  `username` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255),
-  `first_name` VARCHAR(255) NOT NULL,
-  `last_name` VARCHAR(255),
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+CREATE TYPE card_type AS ENUM ('debit', 'credit');
+CREATE TYPE transaction_type AS ENUM ('debit', 'credit');
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS `cards` (
-  `id` SERIAL NOT NULL,
-  `title` VARCHAR(255) NOT NULL,
-  `brand` VARCHAR(255) NOT NULL,
-  `type` ENUM('debit', 'credit') NOT NULL,
-  `description` TEXT,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS cards (
+  id SERIAL NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  brand VARCHAR(255) NOT NULL,
+  type CARD_TYPE NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS `users_cards` (
-  `id` SERIAL NOT NULL,
-  `user_id` INT NOT NULL,
-  `card_id` INT NOT NULL,
-  `last_four` VARCHAR(4) NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
-  FOREIGN KEY (`card_id`) REFERENCES `cards`(`id`)
+CREATE TABLE IF NOT EXISTS users_cards (
+  id SERIAL NOT NULL,
+  user_id INT NOT NULL,
+  card_id INT NOT NULL,
+  last_four VARCHAR(4) NOT NULL,
+  current_limit DECIMAL(10, 2) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (card_id) REFERENCES cards(id)
 );
 
-CREATE TABLE IF NOT EXISTS `transactions` (
-  `id` SERIAL NOT NULL,
-  `user_card_id` INT NOT NULL,
-  `amount` DECIMAL(10, 2) NOT NULL,
-  `transaction_type` ENUM('debit', 'credit') NOT NULL,
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`user_card_id`) REFERENCES `users_cards`(`id`)
+CREATE TABLE IF NOT EXISTS transactions (
+  id SERIAL NOT NULL,
+  user_card_id INT NOT NULL,
+  amount DECIMAL(10, 2) NOT NULL,
+  transaction_type TRANSACTION_TYPE NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_card_id) REFERENCES users_cards(id)
 );
